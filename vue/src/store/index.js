6656f4a6cf365a5e7851e2da159606d2df3c8bb4
@ahmed_kamel <ahmedkelany255@gmail.com>
2022-03-24 17:58:10 +0200

    inegrating larvel with vue and adminlte

---
+++ vue/src/store/index.js
@@ -0,0 +1,16 @@
+import {createStore} from "vuex"
+
+const store = createStore({
+    state:{
+        user:{
+            data:{name:"kelnay"},
+            token:null
+        }
+    },
+    mutations:{},
+    getters:{},
+    actions:{},
+    modules:{}
+})
+
+export default store;
